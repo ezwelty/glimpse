b9e57811fa6b3092a058dae79de183fd6bbee182
ezwelty <ethan.welty@gmail.com>
2017-09-10 16:53:53 -0600

    Add Image class

---
+++ Image.py
@@ -0,0 +1,155 @@
+from datetime import datetime
+import numpy as np
+import exifread
+import scipy.misc
+from Camera import Camera
+
+class Image(object):
+    """
+    An `Image` describes the camera settings and resulting image captured at a particular time.
+
+    Attributes:
+        path (str): Path to the image file
+        time (datetime): Capture date and time
+        size (array): Size of the original image in pixels [nx, ny]
+        shutter (float): Shutter speed in seconds
+        aperture (float): Lens aperture
+        iso (float): Film speed
+        cam (Camera): Camera object
+    """
+    
+    def __init__(self, path, cam_props = None):
+        """Create an Image."""
+        self.path = path
+        # Load image metadata
+        f = open(path, 'rb')
+        self.info = exifread.process_file(f, details=False)
+        # Camera object
+        if not cam_props:
+            cam_props = {}
+        if cam_props.has_key('imgsz'):
+            raise UserWarning("cam_props['imgsz'] replaced by actual image size")
+        cam_props['imgsz'] = self.size
+        if not cam_props.has_key('f'):
+            cam_props['f'] = fmm_to_fpx(self.fmm, self.sensorsz, self.size)
+        self.cam = Camera(**cam_props)
+    
+    @property
+    def size(self):
+        imgsz = [
+            parse_exif_tag(self.info['EXIF ExifImageWidth']),
+            parse_exif_tag(self.info['EXIF ExifImageLength'])
+        ]
+        return(np.array(imgsz))
+    
+    @property
+    def datetime(self):
+        datetime_str = parse_exif_tag(self.info['EXIF DateTimeOriginal'])
+        subsec_str = parse_exif_tag(self.info['EXIF SubSecTimeOriginal'])
+        return(datetime.strptime(datetime_str + "." + subsec_str.zfill(6), "%Y:%m:%d %H:%M:%S.%f"))
+        
+    @property
+    def epoch(self):
+        dt = self.datetime - datetime(1970, 1, 1)
+        return(dt.total_seconds())
+    
+    @property
+    def shutter(self):
+        return(parse_exif_tag(self.info['EXIF ExposureTime']))
+        
+    @property
+    def aperture(self):
+        return(parse_exif_tag(self.info['EXIF FNumber']))
+        
+    @property
+    def iso(self):
+        return(parse_exif_tag(self.info['EXIF ISOSpeedRatings']))
+        
+    @property
+    def fmm(self):
+        return(parse_exif_tag(self.info['EXIF FocalLength']))
+        
+    @property
+    def make(self):
+        return(parse_exif_tag(self.info['Image Make']))
+        
+    @property
+    def model(self):
+        return(parse_exif_tag(self.info['Image Model']))
+    
+    @property
+    def sensorsz(self):
+        sensorsz = get_sensor_size(self.make, self.model)
+        return(np.array(sensorsz))
+    
+    def read():
+        """Read image file."""
+        I = scipy.misc.imread(self.path)
+        return(I)
+
+def parse_exif_tag(tag):
+    """Parse the value of a tag returned by exifread.process_file()."""
+    value = tag.values
+    if type(value) is list:
+        value = value[0]
+    if isinstance(value, exifread.Ratio):
+        return(float(value.num) / value.den)
+    else:
+        return(value)
+
+def get_sensor_size(make, model):
+    """
+    Get a camera model's CCD sensor width and height in mm.
+    
+    Data is from Digital Photography Review (https://dpreview.com).
+    See also https://www.dpreview.com/articles/8095816568/sensorsizes.
+    
+    Arguments:
+        make (str): Camera make (EXIF Make)
+        model (str): Camera model (EXIF Model)
+        
+    Return:
+        list: Camera sensor width and height in mm
+    """
+    make_model = make.strip() + " " + model.strip()
+    sensor_sizes = { # mm
+        'NIKON CORPORATION NIKON D2X': [23.7, 15.7], # https://www.dpreview.com/reviews/nikond2x/2
+        'NIKON CORPORATION NIKON D200': [23.6, 15.8], # https://www.dpreview.com/reviews/nikond200/2
+        'NIKON CORPORATION NIKON D300S': [23.6, 15.8], # https://www.dpreview.com/reviews/nikond300s/2
+        'NIKON E8700': [8.8, 6.6], # https://www.dpreview.com/reviews/nikoncp8700/2
+        'Canon Canon EOS 20D': [22.5, 15.0], # https://www.dpreview.com/reviews/canoneos20d/2
+        'Canon Canon EOS 40D': [22.2, 14.8], # https://www.dpreview.com/reviews/canoneos40d/2
+    }
+    if sensor_sizes.has_key(make_model):
+        return(sensor_sizes[make_model])
+    else:
+        raise KeyError("No sensor size found for " + make_model)
+
+def fmm_to_fpx(fmm, sensorsz, imgsz):
+    """Convert focal length in millimeters to pixels."""
+    return(fmm * imgsz / sensorsz)
+    
+def fpx_to_fmm(fpx, sensorsz, imgsz):
+    """Convert focal length in pixels to millimeters."""
+    return(fpx * sensorsz / imgsz)
+
+# def image_size_to_scale(original_size, target_size):
+#     """Calculate the scale that achieves a target size."""
+#     original_size = np.array(original_size).round()
+#     target_size = np.array(target_size).round()
+#     scale_bounds = target_size / original_size;
+    
+    
+#     if np.array_equal(original_size, target_size):
+#         return(float(1))
+    
+#      if all(original_size == target_size)
+#         scale = 1;
+#         return
+#       end
+#       scale_bounds = target_size ./ original_size;
+#       error_function = @(scale) sum(abs(round(original_size * scale) - target_size));
+#       [scale, value] = fminbnd(error_function, min(scale_bounds), max(scale_bounds));
+#       if value > 0
+#         error('No scale can achieve the target size.');
+#       end

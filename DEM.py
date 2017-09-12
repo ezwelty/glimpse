1f71fc346b661a2ce4ec50120ee0b65079a44430
ezwelty <ethan.welty@gmail.com>
2017-09-12 01:44:59 -0600

    Add DEM class with tests

---
+++ DEM.py
@@ -0,0 +1,371 @@
+import operator
+import numpy as np
+import scipy.interpolate
+import scipy.ndimage.filters
+import scipy.misc
+
+class DEM(object):
+    """
+    A `DEM` describes elevations on a regular 2-dimensional grid.
+    
+    Attributes:
+        Z (array): Grid of values on a regular xy grid
+        zlim (array): Limits of `Z` [min, max]
+        xlim,ylim (array): Outer bounds of the grid [left, right], [top, bottom]
+        x,y (array): Cell center coordinates as row vectors [left to right], [top to bottom]
+        X,Y (array): Cell center coordinates as grids equivalent to `Z`
+        min (array): Minimum bounding box coordinates [x, y, z]
+        max (array): Maximum bounding box coordinates [x, y, z]
+        n (array): Dimensions of `Z` [nx|cols, ny|rows]
+        d (array): Grid cell size [dx, dy]
+        datetime (datetime): Capture date and time
+    """
+    
+    def __init__(self, Z, x=None, y=None, datetime=None):
+        """
+        Create a `DEM`.
+        
+        Arguments:
+            Z (array): Grid of values on a regular xy grid
+            x (object): Either `xlim`, `x`, or `X`
+            y (object): Either `ylim`, `y`, or `Y`
+        """
+        self.Z = Z
+        self._Zf = None
+        self.xlim, self._x, self._X = self._parse_x(x)
+        self.ylim, self._y, self._Y = self._parse_y(y)
+        self.datetime = datetime
+    
+    Z = property(operator.attrgetter('_Z'))
+    
+    @Z.setter
+    def Z(self, value):
+        if hasattr(self, '_Z'):
+            original_shape = self._Z.shape
+            self._Z = np.asarray(value, float)
+            self._clear_cache(['Zf'])
+            if self._Z.shape != original_shape:
+                self._clear_cache(['x', 'X', 'y', 'Y'])
+        else:
+            self._Z = np.asarray(value, float)
+    
+    # ---- Properties (dependent) ----
+    
+    @property
+    def zlim(self):
+        value = [self.Z.min(), self.Z.max()]
+        return np.array(value)
+    
+    @property
+    def min(self):
+        value = [min(self.xlim), min(self.ylim), min(self.zlim)]
+        return np.array(value)
+    
+    @property
+    def max(self):
+        value = [max(self.xlim), max(self.ylim), max(self.zlim)]
+        return np.array(value)
+    
+    @property
+    def n(self):
+        value = [self.Z.shape[1], self.Z.shape[0]]
+        return np.asarray(value, int)
+    
+    @property
+    def d(self):
+        return np.append(np.diff(self.xlim), np.diff(self.ylim)) / self.n
+    
+    # ---- Properties (cached) ----
+    
+    @property
+    def x(self):
+        if self._x is None:
+            value = np.linspace(self.min[0] + abs(self.d[0]) / 2, self.max[0] - abs(self.d[0]) / 2, self.n[0])
+            if self.d[0] < 0:
+                self._x = value[::-1]
+            else:
+                self._x = value
+        return self._x
+    
+    @property
+    def X(self):
+        if self._X is None:
+            self._X = np.tile(self.x, [self.n[1], 1])
+        return self._X
+    
+    @property
+    def y(self):
+        if self._y is None:
+            value = np.linspace(self.min[1] + abs(self.d[1]) / 2, self.max[1] - abs(self.d[1]) / 2, self.n[1])
+            if self.d[1] < 0:
+                self._y = value[::-1]
+            else:
+                self._y = value
+        return self._y
+    
+    @property 
+    def Y(self):
+        if self._Y is None:
+            self._Y = np.tile(self.y, [self.n[0], 1]).transpose()
+        return self._Y
+    
+    @property
+    def Zf(self):
+        if self._Zf is None:
+            sign = np.sign(self.d).astype(int)
+            self._Zf = scipy.interpolate.RectBivariateSpline(
+                self.x[::sign[0]], self.y[::sign[1]], np.nan_to_num(self.Z[::sign[1], ::sign[0]]).T, kx=3, ky=3, s=0
+                )
+        return self._Zf
+              
+    # ---- Methods (public) ----
+    
+    def inbounds(self, xy):
+        """
+        Test whether points are within (or on) the xy bounds of the `DEM`.
+        
+        Arguments:
+            xy (array): Point coordinates (Nx2)
+        
+        Returns:
+            array: Whether each point is inbounds (Nx1)
+        """
+        return np.logical_and(xy >= self.min[0:2], xy <= self.max[0:2]).all(axis = 1)
+    
+    def sample(self, xy):
+        """
+        Sample `Z` at points.
+        
+        Interpolation is performed using a 3rd order spline.
+        Missing values are currently replaced with zeros.
+        
+        Arguments:
+            xy (array): Point coordinates (Nx2)
+        
+        Returns:
+            array: Value of `Z` interpolated at each point (Nx1).
+        """
+        return self.Zf(xy[:, 0], xy[:, 1], grid=False)
+    
+    def sample_grid(self, x, y):
+        """
+        Sample `Z` at points.
+        
+        Interpolation is performed using a 3rd order spline.
+        Missing values are currently replaced with zeros.
+        
+        Arguments:
+            x, y (array_like): Coordinates specifying points on a grid.
+                Arrays must be sorted in increasing order.
+        
+        Returns:
+            array: Value of `Z` interpolated at each point, with x as columns and y as rows.
+        """
+        return self.Zf(x, y, grid=True).transpose()
+    
+    def crop(self, xlim=None, ylim=None, copy=True):
+        """
+        Crop a `DEM`.
+        
+        Arguments:
+            xlim (array_like): Cropping bounds in x 
+            ylim (array_like): Cropping bounds in y
+            copy (bool): Whether to return a copy of the input `DEM`
+        
+        Returns:
+            DEM: New object (if `copy=True`)
+        """
+        if xlim is None and ylim is None:
+            if copy:
+                return self
+        # Intersect xlim
+        if xlim is None:
+            xlim = self.xlim
+        else:
+            xlim = np.asarray([
+                max(min(self.xlim), min(xlim)),
+                min(max(self.xlim), max(xlim))
+                ], float)
+            dx = np.diff(xlim)
+            if dx <= 0:
+                raise ValueError("Crop bounds (xlim) do not intersect DEM.")
+            if np.diff(self.xlim) < 0:
+                xlim = xlim[::-1]
+        # Intersect ylim
+        if ylim is None:
+            ylim = self.ylim
+        else:
+            ylim = np.asarray([
+                max(min(self.ylim), min(ylim)),
+                min(max(self.ylim), max(ylim))
+                ], float)
+            dy = np.diff(ylim)
+            if dy <= 0:
+                raise ValueError("Crop bounds (ylim) do not intersect DEM.")
+            if np.diff(self.ylim) < 0:
+                ylim = ylim[::-1]
+        # Convert xy limits to grid indices
+        xy = np.array([xlim, ylim]).transpose()
+        rc = self._xy_to_rowcol(xy)
+        is_not_edge = (xy[1, :] % self.d != 0)[::-1]
+        # Crop DEM
+        Z = self.Z[rc[0, 0]:rc[1, 0] + int(is_not_edge[0]), rc[0, 1]:rc[1, 1] + int(is_not_edge[1])]
+        new_xy = self._rowcol_to_xy(rc)
+        new_xlim = new_xy[:, 0]
+        if not np.diff(new_xlim):
+            new_xlim[1] += self.d[0]
+        new_ylim = new_xy[:, 1]
+        if not np.diff(new_ylim):
+            new_ylim[1] += self.d[1]
+        if copy:
+            return DEM(Z, new_xlim, new_ylim)
+        else:
+            self.Z = Z
+            self.xlim = new_xlim
+            self.ylim = new_ylim
+            
+    def resize(self, scale, copy=True):
+        """
+        Resize a `DEM`.
+        
+        Arguments:
+            scale (float): Fraction of current size
+            copy (bool): Whether to return a copy of the input `DEM`
+        
+        Returns:
+            DEM: New object (if `copy=True`)
+        """
+        Z = scipy.misc.imresize(self.Z, size=float(scale), interp='bilinear')
+        if copy:
+            return DEM(Z, self.xlim, self.ylim)
+        else:
+            self.Z = Z
+    
+    def fill_crevasses_simple(self, maximum_filter_size=5, gaussian_filter_sigma=5, copy=True):
+        """
+        Apply a maximum filter to `Z`, then perform Gaussian smoothing (fast).
+        
+        Arguments:
+            maximum_filter_size (int): Kernel size of maximum filter in pixels
+            gaussian_filter_sigma (float): Standard deviation of Gaussian filter
+            copy (bool): Whether to return a copy of the input `DEM`
+        
+        Returns:
+            DEM: New object (if `copy=True`)
+        """
+        Z = scipy.ndimage.filters.gaussian_filter(
+            scipy.ndimage.filters.maximum_filter(self.Z, size=maximum_filter_size),
+            sigma=gaussian_filter_sigma
+            )
+        if copy:
+            xs = [self._X, self._x, self.xlim]
+            x = next(item for item in xs if item is not None)
+            ys = [self._Y, self._y, self.ylim]
+            y = next(item for item in ys if item is not None)
+            return DEM(Z, x, y)
+        else:
+            self.Z = Z
+        
+    def fill_crevasses_complex(self, maximum_filter_size=5, gaussian_filter_sigma=5, copy=True):
+        """
+        Find the local maxima of `Z`, fit a surface through them, then perform Gaussian smoothing (slow).
+        
+        Arguments:
+            maximum_filter_size (int): Kernel size of maximum filter in pixels
+            gaussian_filter_sigma (float): Standard deviation of Gaussian filter
+            copy (bool): Whether to return a copy of the input `DEM`
+            
+        Returns:
+            DEM: New object (if `copy=True`)
+        """
+        Z_maxima = scipy.ndimage.filters.maximum_filter(self.Z, size=maximum_filter_size)
+        is_max = (Z_maxima == self.Z).ravel()
+        Xmax = self.X.ravel()[is_max]
+        Ymax = self.Y.ravel()[is_max]
+        Zmax = self.Z.ravel()[is_max]
+        max_interpolant = scipy.interpolate.LinearNDInterpolator(np.vstack((Xmax, Ymax)).T, Zmax)
+        Z_fmax = max_interpolant(self.X.ravel(), self.Y.ravel()).reshape(self.Z.shape)
+        Z = scipy.ndimage.filters.gaussian_filter(Z_fmax, sigma=gaussian_filter_sigma)
+        if copy:
+            xs = [self._X, self._x, self.xlim]
+            x = next(item for item in xs if item is not None)
+            ys = [self._Y, self._y, self.ylim]
+            y = next(item for item in ys if item is not None)
+            return DEM(Z, x, y)
+        else:
+            self.Z = Z
+    
+    # ---- Methods (private) ----
+    
+    def _clear_cache(self, attributes=['x', 'X', 'y', 'Y', 'Zf']):
+        for attr in attributes:
+            setattr(self, '_' + attr, None)
+    
+    def _xy_to_rowcol(self, xy):
+        origin = np.append(self.xlim[0], self.ylim[0])
+        colrow = np.floor((xy - origin) / self.d).astype(int)
+        return colrow[:, ::-1]
+    
+    def _rowcol_to_xy(self, rowcol):
+        colrow = rowcol[:, ::-1]
+        origin = np.append(self.xlim[0], self.ylim[0])
+        xy = colrow * self.d + origin
+        return xy
+    
+    def _parse_x(self, obj):
+        """
+        Parse object into xlim, x, and X attributes.
+        
+        Arguments:
+            obj (object): Either xlim, x, or X
+        """
+        if obj is None:
+            obj = [0, self.n[0]]
+        if type(obj) is not np.ndarray:
+            obj = np.asarray(obj, float)
+        is_X = len(obj.shape) > 1 and all(n > 1 for n in obj.shape[0:2])
+        if is_X:
+            # TODO: Check if all rows equal
+            X = obj
+            obj = obj[0, :]
+        else:
+            X = None
+        is_x = any(n > 2 for n in obj.shape[0:2])
+        if is_x:
+            x = obj
+            # TODO: Check if equally spaced monotonic
+            dx = abs(np.diff(obj[0:2]))
+            xlim = np.append(obj[0] - dx / 2, obj[-1] + dx / 2)
+        else:
+            x = None
+            xlim = obj
+        return [xlim, x, X]
+    
+    def _parse_y(self, obj):
+        """
+        Parse object into ylim, y, and Y attributes.
+        
+        Arguments:
+            obj (object): Either ylim, y, or Y
+        """
+        if obj is None:
+            obj = [self.n[1], 0]
+        if type(obj) is not np.ndarray:
+            obj = np.asarray(obj, float)
+        is_Y = len(obj.shape) > 1 and all(n > 1 for n in obj.shape[0:2])
+        if is_Y:
+            # TODO: Check if all rows equal
+            Y = obj
+            obj = obj[:, 0]
+        else:
+            Y = None
+        is_y = any(n > 2 for n in obj.shape[0:2])
+        if is_y:
+            y = obj
+            # TODO: Check if equally spaced monotonic
+            dy = abs(np.diff(obj[0:2]))
+            ylim = np.append(obj[0] + dy / 2, obj[-1] - dy / 2)
+        else:
+            y = None
+            ylim = obj
+        return [ylim, y, Y]

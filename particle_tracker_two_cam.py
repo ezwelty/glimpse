9da04d004d0d84f4fc2acdd5f6a0d841fb086853
brinkerhoff <douglas.brinkerhoff@gmail.com>
2017-10-23 17:06:55 -0600

    Learning how to use git again...

--- particle_tracker_two_cam.py
+++ particle_tracker_two_cam.py
@@ -9,75 +9,17 @@ import sys
 import cv2 # only for matchTemplate()
 
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec # unused
 import scipy.interpolate as si
 import scipy.optimize as so
 import scipy.misc as sm
 import scipy.spatial as ssp
 import scipy.ndimage.filters as filts
-import scipy.ndimage as snd # unused
 import skimage.feature as skif
-import filterpy.kalman as fpk # unused
 import sklearn.decomposition as sde
 import matplotlib.patches as patches
 
-import multiprocess as mp # unused
-import camera # unused
 import sharedmem
-
-from fractions import gcd # unused
-
-# Save and load commands for efficient pickle objects
-def save_zipped_pickle(obj, filename, protocol=-1):
-    with gzip.open(filename, 'wb') as f:
-        cPickle.dump(obj, f, protocol)
-
-def load_zipped_pickle(filename):
-    with gzip.open(filename, 'rb') as f:
-        loaded_object = cPickle.load(f)
-        return loaded_object
-
-def hist_match(source, template):
-    """
-    Adjust the pixel values of a grayscale image such that its histogram
-    matches that of a target image
-
-    Arguments:
-    -----------
-        source: np.ndarray
-            Image to transform; the histogram is computed over the flattened
-            array
-        template: np.ndarray
-            Template image; can have different dimensions to source
-    Returns:
-    -----------
-        matched: np.ndarray
-            The transformed output image
-    """
-
-    oldshape = source.shape
-    source = source.ravel()
-    template = template.ravel()
-
-    # get the set of unique pixel values and their corresponding indices and
-    # counts
-    s_values, bin_idx, s_counts = np.unique(source, return_inverse=True,
-                                            return_counts=True)
-    t_values, t_counts = np.unique(template, return_counts=True)
-
-    # take the cumsum of the counts and normalize by the number of pixels to
-    # get the empirical cumulative distribution functions for the source and
-    # template images (maps pixel value --> quantile)
-    s_quantiles = np.cumsum(s_counts).astype(np.float64)
-    s_quantiles /= s_quantiles[-1]
-    t_quantiles = np.cumsum(t_counts).astype(np.float64)
-    t_quantiles /= t_quantiles[-1]
-
-    # interpolate linearly to find the pixel values in the template image
-    # that correspond most closely to the quantiles in the source image
-    interp_t_values = np.interp(s_quantiles, t_quantiles, t_values)
-
-    return interp_t_values[bin_idx].reshape(oldshape)
+from helper import *
 
 class ParticleTracker(object):
     """

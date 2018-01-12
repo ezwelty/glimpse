648cc79d907ded805ed6da928abbc1fd003745a6
ezwelty <ethan.welty@gmail.com>
2018-01-11 20:45:22 -0700

    Calibrate pair of CG05 star images

--- cg-calibrations/calibrate_images.py
+++ cg-calibrations/calibrate_images.py
@@ -18,9 +18,9 @@ SVG_KEYS = ['gcp', 'horizon', 'coast', 'terminus', 'moraines']
 
 # STATION = 'AKJNC'
 # SVG_KEYS = ['gcp', 'horizon', 'coast', 'moraines']
-STATION = 'AKST03B'
+STATION = 'CG05'
 
-svg_paths = glob.glob("svg/" + STATION + "_*.svg")
+svg_paths = glob.glob("svg/" + STATION + "_20050827_130000.svg")
 for path in svg_paths:
     for suffix in ['']:
         ids = cgcalib.parse_image_path(path)

3138443e22d68e2acb4471ef9befa4839c82b35d
ezwelty <ethan.welty@gmail.com>
2018-03-20 10:40:46 -0600

    Push missing parallel module

---
+++ glimpse/parallel.py
@@ -0,0 +1,51 @@
+from .imports import (np, sharedmem, cv2)
+from . import (helpers, optimize, image)
+
+def track(tracker, xy, n, xy_sigma, vxy=(0, 0), vxy_sigma=(0, 0),
+    datetimes=None, maxdt=0, tile_size=(15, 15), axy=(0, 0), axy_sigma=(0, 0)):
+    """
+    Run Tracker.track() in parallel for multiple initial particle states.
+
+    Requires image.SHAREDMEM_COMPATIBLE to be True (for Ethan), which disables
+    use of numpy.matmul in Camera._world2camera().
+    """
+    SHAREDMEM_COMPATIBLE = image.SHAREDMEM_COMPATIBLE
+    if not SHAREDMEM_COMPATIBLE:
+        image.SHAREDMEM_COMPATIBLE = True
+    for obs in tracker.observers:
+        obs.cache_images()
+    def process(xyi):
+        tracker.initialize_particles(
+            n=n, xy=xyi, xy_sigma=xy_sigma, vxy=vxy, vxy_sigma=vxy_sigma)
+        tracker.track(
+            datetimes=datetimes, maxdt=maxdt, tile_size=tile_size,
+            axy=axy, axy_sigma=axy_sigma)
+        return np.vstack(tracker.means), np.dstack(tracker.covariances)
+    with sharedmem.MapReduce() as pool:
+        results = pool.map(process, xy)
+    if not SHAREDMEM_COMPATIBLE:
+        image.SHAREDMEM_COMPATIBLE = False
+    return results
+
+def detect_keypoints(arrays, masks=None, paths=None, method='sift', root=True,
+    **params):
+    """
+    Detect keypoints and descriptors in parallel for multiple image arrays.
+
+    Does not currently work (for Ethan) for unknown reasons related to
+    cv2.SIFT(SURF).detectAndCompute() clashing with glimpse in parallel.
+    """
+    if masks is np.ndarray or masks is None:
+        masks = (masks, ) * len(arrays)
+    def process(array, mask, path=None):
+        keypoints = optimize.detect_keypoints(
+            array, mask=mask, method=method, root=root, **params)
+        if path:
+            helpers.write_pickle(keypoints, path)
+        else:
+            return keypoints
+    with sharedmem.MapReduce() as pool:
+        if paths:
+            return pool.map(process, zip(arrays, masks, paths), star=True)
+        else:
+            return pool.map(process, zip(arrays, masks), star=True)

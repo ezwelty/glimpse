0bb7f7ae255e6762d495f24d3b4c577fc6cc9b13
ezwelty <ethan.welty@gmail.com>
2018-04-14 09:30:56 -0600

    Remove parallel tracking from parallel module

--- glimpse/parallel.py
+++ glimpse/parallel.py
@@ -1,23 +1,7 @@
 from __future__ import (print_function, division, unicode_literals)
 from .backports import *
-from .imports import (np, sharedmem, cv2)
+from .imports import (np, sharedmem, cv2, datetime)
 from . import (helpers, optimize, image)
-
-def track(tracker, xy, n, xy_sigma, vxy=(0, 0), vxy_sigma=(0, 0),
-    datetimes=None, maxdt=0, tile_size=(15, 15), axy=(0, 0), axy_sigma=(0, 0),
-    processes=None):
-    """
-    Run Tracker.track() in parallel for multiple initial particle states.
-    """
-    def process(xyi):
-        tracker.initialize_particles(
-            n=n, xy=xyi, xy_sigma=xy_sigma, vxy=vxy, vxy_sigma=vxy_sigma)
-        tracker.track(
-            datetimes=datetimes, maxdt=maxdt, tile_size=tile_size,
-            axy=axy, axy_sigma=axy_sigma)
-        return np.vstack(tracker.means), np.dstack(tracker.covariances)
-    with sharedmem.MapReduce(np=processes) as pool:
-        return pool.map(process, xy)
 
 def build_keypoints(matcher, masks=None, overwrite=False,
     clear_images=True, clear_keypoints=False, processes=None, **params):

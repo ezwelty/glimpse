ezwelty/glimpse

commit b328d5ba2bf014916dbc3fea8d2274af0643cb0b

Load world control by attribute rather than class

glimpse/optimize.py
@@ -1336,10 +1336,10 @@ class Cameras(object):
                     and cam is control.cam
                     and not control.directions
                 ):
-                    if isinstance(control, Points):
+                    if hasattr(control, "xyz"):
                         xyz.append(control.xyz)
-                    elif isinstance(control, Lines):
-                        xyz.extend(control.xyzs)
+                    elif hasattr(control, "xyzs"):
+                        xyz.append(control.xyz)
             if xyz:
                 # NOTE: Upper bound (assumes motion perpendicular to feature direction)
                 dpixels[0:3] = (

commit 97361a1ecb9ad9e7f19d51c6b5e8ee5c2cdc34ad

Fix bug introduced in b328d5b

glimpse/optimize.py
@@ -1339,7 +1339,7 @@ class Cameras(object):
                     if hasattr(control, "xyz"):
                         xyz.append(control.xyz)
                     elif hasattr(control, "xyzs"):
-                        xyz.append(control.xyz)
+                        xyz.extend(control.xyzs)
             if xyz:
                 # NOTE: Upper bound (assumes motion perpendicular to feature direction)
                 dpixels[0:3] = (
8f334ab76cb886fdad6ccac81a5abd393b8a9044
ezwelty <ethan.welty@gmail.com>
2020-11-06 12:30:15 -0700

    Add type annotations and docstrings to image test module

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,12 +1,15 @@
+"""Tests of the image module."""
 import datetime
 import os
 
 import glimpse
 import numpy as np
 
+path = os.path.join("tests", "AK10b_20141013_020336.JPG")
 
-def test_image_init_defaults():
-    path = os.path.join("tests", "AK10b_20141013_020336.JPG")
+
+def test_image_init_defaults() -> None:
+    """Initializes with default values loaded from file."""
     img = glimpse.Image(path)
     assert img.path == path
     assert img.datetime == img.exif.datetime
@@ -16,8 +19,8 @@ def test_image_init_defaults():
     )
 
 
-def test_image_init_custom():
-    path = os.path.join("tests", "AK10b_20141013_020336.JPG")
+def test_image_init_custom() -> None:
+    """Initializes with custom values overriding the file."""
     args = {
         "cam": {"imgsz": (100, 100), "sensorsz": (10, 10)},
         "datetime": datetime.datetime(2010, 1, 1),
@@ -31,31 +34,31 @@ def test_image_init_custom():
     )
 
 
-def test_image_read():
-    path = os.path.join("tests", "AK10b_20141013_020336.JPG")
+def test_image_read() -> None:
+    """Reads raster data from file."""
     # Default size
     img = glimpse.Image(path)
-    I = img.read()
-    np.testing.assert_equal(I.shape[0:2][::-1], img.cam.imgsz)
+    A = img.read()
+    np.testing.assert_equal(A.shape[0:2][::-1], img.cam.imgsz)
     # Subset (cached)
     x, y, w, h = 0, 5, 100, 94
     box = x, y, x + w, y + h
-    i = img.read(box, cache=True)
-    assert i.shape[0:2][::-1] == (w, h)
-    np.testing.assert_equal(i, I[y : (y + h), x : (x + w)])
+    a = img.read(box, cache=True)
+    assert a.shape[0:2][::-1] == (w, h)
+    np.testing.assert_equal(a, A[y : (y + h), x : (x + w)])
     # Subset (not cached)
-    img.I = None
-    inc = img.read(box, cache=False)
-    np.testing.assert_equal(i, inc)
+    img = glimpse.Image(path)
+    a_nc = img.read(box, cache=False)
+    np.testing.assert_equal(a, a_nc)
     # Resize camera
     img.cam.resize(0.5)
-    I = img.read()
-    np.testing.assert_equal(I.shape[0:2][::-1], img.cam.imgsz)
+    A = img.read()
+    np.testing.assert_equal(A.shape[0:2][::-1], img.cam.imgsz)
 
 
-def test_image_project():
-    path = os.path.join("tests", "AK10b_20141013_020336.JPG")
+def test_image_project() -> None:
+    """Projects itself into a camera."""
     img = glimpse.Image(path)
     img.cam.resize(0.1)
-    I = img.project(img.cam, method="nearest")
-    np.testing.assert_equal(I[1:], img.read()[1:])
+    A = img.project(img.cam, method="nearest")
+    np.testing.assert_equal(A[1:], img.read()[1:])

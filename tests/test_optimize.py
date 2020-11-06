40291102b9d460dd6782dc37eae06a93e3b1a370
ezwelty <ethan.welty@gmail.com>
2020-11-06 13:14:06 -0700

    Add type annotations and docstrings to optimize test module

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -1,21 +1,31 @@
+"""Tests of the optimize module."""
 import copy
 import os
 
 import glimpse
 import glimpse.optimize
-import numpy as np
 
 
-def test_ransac_camera_viewdir():
+def test_optimizes_camera_viewdir_with_ransac() -> None:
+    """
+    Optimizes relative camera view directions with RANSAC.
+
+    Simulates a camera rotation by projecting the image into a rotated camera.
+    Detects and matches keypoints between the original and synthetic image,
+    filters the matches with RANSAC,
+    and solves for the rotation.
+    """
     path = os.path.join("tests", "AK10b_20141013_020336.JPG")
     imgA = glimpse.Image(path)
     imgA.cam.resize(0.5)
     imgB = copy.deepcopy(imgA)
     viewdir = (2, 2, 2)
     imgB.cam.viewdir = viewdir
-    imgB.I = imgA.project(imgB.cam)
     # Match features
-    keypoints = [glimpse.optimize.detect_keypoints(img.read()) for img in (imgA, imgB)]
+    keypoints = [
+        glimpse.optimize.detect_keypoints(a)
+        for a in (imgA.read(), imgA.project(imgB.cam))
+    ]
     uvs = glimpse.optimize.match_keypoints(*keypoints, max_ratio=0.8)
     matches = glimpse.optimize.Matches(cams=(imgA.cam, imgB.cam), uvs=uvs)
     model = glimpse.optimize.Cameras(
